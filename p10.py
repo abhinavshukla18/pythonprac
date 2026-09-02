45f8c42d30527b2f85bd2d6bc86d74499b1a2dfc
abhinavshukla18 <abhinavshukla1802@gmail.com>
2026-09-03 00:15:52 +0530

    Questions on Dict

---
+++ p10.py
@@ -0,0 +1,50 @@
+#PROGRAMS ON DICTIONARIES:-
+
+#1. Print a price list:
+
+#prices = {"pen": 10, "book": 50, "bag": 200}
+#for i, p in prices.items():
+#    print(f"{i} costs {p}")
+
+
+
+#2. Phone Book:
+
+#book = {"Ronaldo": 2342783423, "Suarez": 7894776540, "Camara": 9945994550}
+#ip = input("Enter the name: ")
+#num = book.get(ip, "Not Found")
+#print(num)
+
+
+
+#3. Total & Highest:
+
+#marks = {"math": 90, "sci": 85, "eng": 78}
+#total = 0
+#highest_score = 0
+#highest_sub = -1
+#for sub, score in marks.items():
+#    total = total + score
+#    if score > highest_score:
+#        highest_score = score
+#        highest_sub = sub
+#print(f"total is {total}")
+#print (f"highest score is {highest_score} in {highest_sub}")
+
+
+
+#4. Nested Student Record:
+
+#rec = {"Abhi":8, "Ananya": 9, "Abhinav": 10}
+#highest_gpa = 0
+#highest_name = None
+#for name, gpa in rec.items():
+#    print(f"{name} score {gpa} gpa")
+#    if gpa > highest_gpa:
+#        highest_gpa = gpa
+#        highest_name = name
+#print(f"{highest_name} scored the highest gpa with {highest_gpa}")
+
+
+
+#5. 
