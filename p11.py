9d4747dc69e60a43a377ee5f402997a3274e2db8
abhinavshukla18 <abhinavshukla1802@gmail.com>
2026-09-05 15:31:41 +0530

    Function practice prog

---
+++ p11.py
@@ -0,0 +1,57 @@
+# Practice Programs on Functions:- 
+
+#1. Greet with a Default:
+
+#def greet(name, msg="Welcome"):
+#    print("Hey",name,",", msg)
+#
+#greet("Abhinav")
+#greet("Espi", "You played well")
+
+
+#2. Rectangle Area:
+
+#def area(length, width):
+#    return length*width
+#
+#total = area(4, 10)
+#print("Area of the Rectangle is", total)
+
+
+#3. Is Even?:
+
+#def is_even(n):
+#    return n%2==0
+#
+#test = [0, 4, 7, -2, -5, 12, 19]
+#
+#for num in test:
+#    print(f"is_even({num}) -> {is_even(num)}")
+
+
+
+#4. Simple Interest:
+
+#def interest(principal, rate=5, year=1):
+#    return ( principal * rate * year / 100 )
+#
+#value = interest(3,12,6)
+#print(value)
+
+
+
+#5. Greatest of Three;
+
+#def biggest(a, b, c):
+#    if a >= b and a >= c:
+#        return a
+#    elif b >= a and b >= c:
+#        return b
+#    else:
+#        return c
+#
+#value = biggest(10, 243, 3438284)
+#print(value, "is the greatest")  
+
+
+
