973c80fcf25f1092bc33036825d87da3db517dab
abhinavshukla18 <abhinavshukla1802@gmail.com>
2026-09-13 14:31:38 +0530

    args and kwargs

--- p12.py
+++ p12.py
@@ -1,10 +1,19 @@
 # This is for default arguments, *args and **kwargs:-
 
 #example code for args:-
-def total(*args):
-    print(args)
-    return sum(args)
+#def total(*args):
+#    print(args)
+#    return sum(args)
+#
+#print(total(1,2,3))
+#print(total(10,20,20,30,40))
+#print(total())
 
-print(total(1,2,3))
-print(total(10,20,20,30,40))
-print(total())
+
+#example code for kwargs:-
+def profile(**kwargs):
+    print(kwargs) # kwargs is a dict
+    for key, value in kwargs.items():
+        print(key, "=", value)
+
+profile(name="Amit", age=21, city="Varanasi")
